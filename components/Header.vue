468c79b330e9b7c8697f44d7091b212827c65c44
Allen K Abraham <allenabraham777@gmail.com>
2020-05-07 17:13:47 +0530

    Basic project layout made

--- components/Header.vue
+++ components/Header.vue
@@ -1,48 +1,88 @@
 <template>
   <div>
-    <span>
-      STACS
-    </span>
     <nav>
-      <ul>
-        <li>
-          <nuxt-link to="/committee">
-            Committee
-          </nuxt-link>
-        </li>
-        <li>
-          <nuxt-link to="/faculties">
-            Faculties
-          </nuxt-link>
-        </li>
-        <li>
-          <nuxt-link to="/activities">
-            Activities
-          </nuxt-link>
-        </li>
-        <li>
-          <nuxt-link to="/awards">
-            Awards
-          </nuxt-link>
-        </li>
-        <li>
-          <nuxt-link to="/newsletter">
-            Newsletter
-          </nuxt-link>
-        </li>
-        <li>
-          <nuxt-link to="/gallery">
-            Gallery
-          </nuxt-link>
-        </li>
-      </ul>
+      <div class="nav-wrapper #2979ff blue accent-3">
+        <nuxt-link to="/" class="brand-logo">
+          <img src="@/assets/images/logo.png">
+        </nuxt-link>
+        <nuxt-link to="#" data-target="mobile-demo" class="sidenav-trigger button-collapse">
+          <i class="material-icons">menu</i>
+        </nuxt-link>
+        <ul class="right hide-on-med-and-down">
+          <li>
+            <nuxt-link to="/committee">
+              Committee
+            </nuxt-link>
+          </li>
+          <li>
+            <nuxt-link to="/faculties">
+              Faculties
+            </nuxt-link>
+          </li>
+          <li>
+            <nuxt-link to="/activities">
+              Activities
+            </nuxt-link>
+          </li>
+          <li>
+            <nuxt-link to="/awards">
+              Awards
+            </nuxt-link>
+          </li>
+          <li>
+            <nuxt-link to="/newsletter">
+              Newsletter
+            </nuxt-link>
+          </li>
+          <li>
+            <nuxt-link to="/gallery">
+              Gallery
+            </nuxt-link>
+          </li>
+        </ul>
+      </div>
     </nav>
+
+    <ul id="mobile-demo" class="sidenav">
+      <li>
+        <nuxt-link to="/committee">
+          Committee
+        </nuxt-link>
+      </li>
+      <li>
+        <nuxt-link to="/faculties">
+          Faculties
+        </nuxt-link>
+      </li>
+      <li>
+        <nuxt-link to="/activities">
+          Activities
+        </nuxt-link>
+      </li>
+      <li>
+        <nuxt-link to="/awards">
+          Awards
+        </nuxt-link>
+      </li>
+      <li>
+        <nuxt-link to="/newsletter">
+          Newsletter
+        </nuxt-link>
+      </li>
+      <li>
+        <nuxt-link to="/gallery">
+          Gallery
+        </nuxt-link>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
-
+  mounted () {
+    this.$materialize.sidenav()
+  }
 }
 </script>
 
--- layouts/default.vue
+++ layouts/default.vue
@@ -7,6 +7,7 @@
 
 <script>
 import Header from '~/components/Header'
+
 export default {
   components: {
     Header
