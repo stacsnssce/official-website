fecb2a9b85d9bd9d579bf1571fa868c27253fcb0
Allen K Abraham <allenabraham777@gmail.com>
2020-05-08 14:03:42 +0530

    Markdown Renderer added

---
+++ pages/blog/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <section class="container">
+    <h1><u>Blog Home Page</u></h1>
+    <div class="posts">
+      <div v-for="post in posts" :key="post.id" class="post">
+        <h3>{{ post.heading }}</h3>
+        <h5>{{ post.author }}</h5>
+        <h5>{{ post.date }}</h5>
+        <nuxt-link :to="`/blog/${post.id}`">
+          Read More
+        </nuxt-link>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  async asyncData (context) {
+    return await axios.get('https://raw.githubusercontent.com/stacsnssce/webdata/master/posts/postlist.json')
+      .then(({ data }) => {
+        return {
+          posts: data.Posts
+        }
+      })
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
