d3af5c7f932c514fae4f3f6c1b4d79a9039a62e7
Mohammed Arshu PT <arshupt123@gmail.com>
2020-05-14 20:12:27 +0530

    Fetching Data

--- pages/committee/index.vue
+++ pages/committee/index.vue
@@ -1,12 +1,28 @@
 <template>
-  <div>
+  <section class="container">
+    {{ /*eslint-disable-next-line*/ }}
     Committee Page
-  </div>
+    <div class="posts">
+      <div v-for="post in posts" :key="post.id" class="post">
+        <h4>{{ post.name }}</h4>
+        <h4>{{ post.profiles.github }}</h4>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
-export default {
+import axios from 'axios'
 
+export default {
+  async asyncData (context) {
+    return await axios.get('https://raw.githubusercontent.com/stacsnssce/webdata/master/committee.json')
+      .then(({ data }) => {
+        return {
+          posts: data.committee
+        }
+      })
+  }
 }
 </script>
 
