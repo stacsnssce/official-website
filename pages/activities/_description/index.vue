9e4f1c5ef8c83b9309ac1b0d6b3513eeb3987733
Rinsa Fathima <rinsafathima09@gmail.com>
2020-05-15 15:21:04 +0530

    Added activities page

--- pages/activities/_description/index.vue
+++ pages/activities/_description/index.vue
@@ -1,15 +1,37 @@
 <template>
-  <div>
-    Activity Description
+  <div class="container">
+      {{ /*eslint-disable-next-line*/ }}
+      <div v-html="content" ></div>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+const fm = require('front-matter')
+const md = require('markdown-it')({
+  html: true,
+  typographer: true
+})
 export default {
-
+  async asyncData (context) {
+    if (context) {
+      return await axios.get(`https://raw.githubusercontent.com/stacsnssce/webdata/master/activities/${context.params.description}.md`)
+        .then((res) => {
+          const mdf = fm(res.data)
+          return {
+            activity: mdf.attributes,
+            content: md.render(mdf.body)
+          }
+        })
+    }
+  },
+  data () {
+    return {
+      activity: {}
+    }
+  }
 }
 </script>
 
-<style>
-
+<style lang="scss">
 </style>
--- pages/activities/index.vue
+++ pages/activities/index.vue
@@ -1,14 +1,31 @@
 <template>
-  <div>
-    Activities Page
-  </div>
+  <section class="container">
+    <h1><u>Activities</u></h1>
+    <div class="activities">
+      <div v-for="activity in activities" :key="activity.id" class="activity">
+        <h3>{{ activity.activity }}</h3>
+        <div><img :src=activity.url></div>
+         <nuxt-link :to="`/activities/${activity.id}`">
+         Read more..
+        </nuxt-link>
+        </div>
+    </div>
+  </section>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
-
+  async asyncData (context) {
+    return await axios.get('https://raw.githubusercontent.com/stacsnssce/webdata/master/activities/acticities.json')
+      .then(({ data }) => {
+        return {
+          activities: data
+        }
+      })
+  }
 }
 </script>
 
-<style>
+<style lang="scss">
 </style>
