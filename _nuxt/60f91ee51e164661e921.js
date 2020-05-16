4fe397848c14e7ad947e263b09ca7c352aa35c96
allenabraham777 <allenabraham777@users.noreply.github.com>
2020-05-16 21:55:38 +0000

    deploy: 31055b8c64e7d336477fc8971af2084e8a0528a7

--- _nuxt/60f91ee51e164661e921.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[13],{304:function(t,e,n){"use strict";n.r(e);var r={},l=n(10),component=Object(l.a)(r,(function(){var t=this.$createElement;return(this._self._c||t)("div",[this._v("\n  Newsletter Description\n  "+this._s(this.$route.query.test)+"\n")])}),[],!1,null,null,null);e.default=component.exports}}]);
