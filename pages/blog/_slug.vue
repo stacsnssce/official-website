6864afefa6101836be77134a128e6339080ea55e
Allen K Abraham <allen@theabstraction.co>
2020-05-18 02:48:56 +0530

    Modification in the blog display page

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -10,6 +10,7 @@
 <script>
 /* eslint-disable */
 import axios from 'axios'
+const fm = require('front-matter')
 const md = require('markdown-it')({
   html: true,
   typographer: true
@@ -17,14 +18,30 @@ const md = require('markdown-it')({
 export default {
   async asyncData({params, payload}) {
       if(payload) {
-          return {
-              post: payload
-          }
-      } else {
         return {
-          content: md.render(params.body)
+            post: payload
+        }
+      } else {
+        if(params.body) {
+          return {
+            content: md.render(params.body)
+          }
+        }
+        else {
+          return await axios.get(`https://raw.githubusercontent.com/stacsnssce/webdata/master/posts/${params.slug}.md`)
+            .then((res) => {
+              const mdf = fm(res.data)
+              return {
+                content: md.render(mdf.body)
+              }
+            })
         }
       }
   },
+  data () {
+    return {
+      content: ""
+    }
+  }
 }
 </script>
