189fe037fc9c0847fdbe8afdd129c593c1f353cc
Adharsh <authradam@gmail.com>
2020-05-18 17:31:04 +0530

    Added About Section

--- pages/index.vue
+++ pages/index.vue
@@ -6,6 +6,7 @@ Vue.use(VueCarousel);
   <div class="container-box">
     <div class="container">
       <div class="About">
+        <h2 style="text-align:center">About</h2>
         <p>
           Students' Association of Computer Science (STACS) is the departmental association of both B.Tech and M.Tech students of the Computer Science and Engineering Department. This association will be an integration of students into academic communities at the department level, crossing the course boundary, providing a platform to organise a range of activities with the support and resources of the department. The association will provide opportunities for students to share a range of extra-curricular experiences.
         </p>
