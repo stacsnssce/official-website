4079bcff23f95a3681386e6a47018fd9318a50c3
Allen K Abraham <allen@theabstraction.co>
2020-05-21 02:41:30 +0530

    Activities route rearranged and modified certain rendering

--- pages/activities/_desc.vue
+++ pages/activities/_desc.vue
@@ -16,14 +16,19 @@ const md = require('markdown-it')({
   typographer: true
 })
 export default {
-    async asyncData (context) {
-      if (context) {
-        return await axios.get(`https://raw.githubusercontent.com/stacsnssce/webdata/master/activities/${context.params.description}.md`)
+    async asyncData ({params, payload}) {
+      if(payload) {
+        return {
+            title: payload.title,
+            content: payload.body
+        }
+      } else {
+        return await axios.get(`https://raw.githubusercontent.com/stacsnssce/webdata/master/activities/${params.id}.md`)
           .then((res) => {
             const mdf = fm(res.data)
             
             return {
-              activity: mdf.attributes,
+              title: mdf.attributes,
               content: md.render(mdf.body)
             }
           })
--- pages/activities/index.vue
+++ pages/activities/index.vue
@@ -1,16 +1,30 @@
 <template>
-  <section class="container card activities-wrapper ">
-    <h4 class="page-title center-align"><b>Activities</b></h4>
+  <section class="container card activities-wrapper">
+    <h4 class="page-title center-align">
+      <b>Activities</b>
+    </h4>
     <div class="row activities m3">
       <div v-for="activity in activities" :key="activity.id" class="col s12 m6">
-        <div  class="card activity-card m4">
-          <nuxt-link :to="`/activities/${activity.id}`">
+        <div class="card activity-card m4">
+          <nuxt-link
+            :to="{name: 'activities-desc', params: {desc: activity.desc, id: activity.id}}"
+          >
             <div class="card-image">
-              <img :src=activity.attribute.cover>
+              <progressive-img
+                :src="`${activity.attribute.cover}`"
+                placeholder="/imageplaceholder8x5.png"
+                blur="30"
+                delay="200"
+                class="activity-image center-align"
+              />
             </div>
             <div class="card-content">
-              <h5 class="act-card-title"><b>{{ activity.attribute.title}}</b></h5>
-              <h6 class="act-card-date">{{ activity.attribute.date}}</h6>
+              <h5 class="act-card-title">
+                <b>{{ activity.attribute.title }}</b>
+              </h5>
+              <h6 class="act-card-date">
+                {{ activity.attribute.date }}
+              </h6>
             </div>
           </nuxt-link>
         </div>
@@ -25,37 +39,29 @@ const fm = require('front-matter')
 
 export default {
   fetch ({ store }) {
-    const activities = []
     return axios.get('https://api.github.com/repos/stacsnssce/webdata/contents/activities')
-      .then(({ data }) => {
+      .then(async ({ data }) => {
         /* eslint-disable no-console */
-        console.log(data)
+        // console.log(data)
 
-        data.forEach(async (element) => {
-          await axios.get(element.download_url)
-            .then((res) => {
-              const mdf = fm(res.data)
-              // eslint-disable-next-line
-              console.log(mdf)
-              activities.push({
-                attribute: mdf.attributes,
-                slug: mdf.attributes.title,
-                body: mdf.body,
-                id: element.name.slice(0, -3)
-              })
-            })
-          if (data[data.length - 1] === element) {
-            console.log(`Activities : ${activities}`)
-            store.commit('Activities', activities)
-          }
-        })
-      })
-      .then(() => {
+        store.commit('Activities', await Promise.all(data.map(async (element) => {
+          return await axios.get(element.download_url).then((res) => {
+            const mdf = fm(res.data)
+            // eslint-disable-next-line
+            // console.log(mdf)
+            return {
+              attribute: mdf.attributes,
+              desc: element.sha,
+              body: mdf.body,
+              id: element.name.slice(0, -3)
+            }
+          })
+          // store.commit('Activities', activities)
+        })))
       })
   },
   computed: {
     activities () {
-      console.log(`Activities : ${this.$store.state.activities}`)
       return this.$store.state.activities
     }
   }
@@ -65,41 +71,58 @@ export default {
 </script>
 
 <style lang="scss">
-.activities-wrapper{
+.activity-image {
+  max-width: inherit !important;
+  padding: 0 !important;
+
+  * {
+    padding: 0 !important;
+  }
+
+  .progressive-image-wrapper,
+  .progressive-image-main,
+  .progressive-image-placeholder {
+    max-width: inherit!important;
+    padding: 0 !important;
+  }
+  img {
+    width: 100%;
+    object-fit: cover;
+  }
+}
+.activities-wrapper {
   box-sizing: border-box;
   min-width: 200px;
+
   margin: 0 auto;
-  padding:20px;
-  border-left: 1px solid
-  #eaecef;
-  border-right: 1px solid
-  #eaecef;
-  border-bottom: 1px solid
-  #eaecef;
+  border-left: 1px solid #eaecef;
+  border-right: 1px solid #eaecef;
+  border-bottom: 1px solid #eaecef;
 }
-.page-title{
+.page-title {
   color: #220e24;
 }
-.activities{
+.activities {
   padding: 10px;
 }
-.activity-card{
+.activity-card {
   text-align: center;
   color: white;
-  box-shadow: 0 4px 8px 0 rgba(0,0,0,.2);
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
   cursor: pointer;
   border-radius: 8px;
   padding: 10px;
+  max-width: 450px;
 }
 .activity-card:hover {
   transform: scale(0.98, 0.99);
   box-shadow: 0 0 10px rgba(100, 100, 100, 0.2);
-  transition: 0.3s ;
+  transition: 0.3s;
 }
-.act-card-title{
+.act-card-title {
   color: #0c3c78;
 }
-.act-card-date{
-  color:black;
+.act-card-date {
+  color: black;
 }
 </style>
