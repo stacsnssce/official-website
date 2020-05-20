902b5af17e99b83ffae6c66eb83f075bce52cf2d
allenabraham777 <allenabraham777@users.noreply.github.com>
2020-05-20 21:18:02 +0000

    deploy: 84dfa7171ba35db018c811d66b55df5701fbb921

---
+++ _nuxt/e918641d354d4b8837d4.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{323:function(n,t,e){"use strict";e.r(t);var r={},c=e(10),component=Object(c.a)(r,(function(){var n=this.$createElement;return(this._self._c||n)("div",[this._v("\n  Award Description\n")])}),[],!1,null,null,null);t.default=component.exports}}]);
