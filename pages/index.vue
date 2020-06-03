794a07a081c151c0968f78c08f8b39049bbc2ea5
Adharsh <authradam@gmail.com>
2020-06-03 19:20:41 +0530

    Carousel Couldn't be added

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,28 @@
+import 'materialize-css'
 <template>
-  <div class="bg">
-    <img class="bgimg" src="../assets/images/college.png" alt="Bgimage" />
-    <div class="about">
-      <h1 class="head-about">About</h1>
+  <div class="row">
+    <div class="col-xs-12 col-md-12 col-sm-12 col-lg-12">
       <div class="container">
-        <p class="abtext">Students' Association of Computer Science (STACS) is the departmental association of both B.Tech and M.Tech students of the Computer Science and Engineering Department. This association will be an integration of students into academic communities at the department level, crossing the course boundary, providing a platform to organise a range of activities with the support and resources of the department. The association will provide opportunities for students to share a range of extra-curricular experiences.</p>
+        <h2 class="head1">Students' Association Of Computer Science</h2>
+        <h4 class="head2">Department Of Computer Science and Technology</h4>
       </div>
-      <div class="abtcse">
-        <h1 class="head-csed">About CSED</h1>
+      <img src="../assets/images/colle.jpg" alt class="center" />
+      <div class="about">
         <div class="container">
-        <p class="abcse">The department of computer science & engineering is the latest addition to NSSCE in the academic year 1999 - 2000. Identification of India as a potential source of computer professionals and keeping in mind the employment potential in this sector, the department aims at providing competent professionals to contribute its share to this Technologies boom. Faculty members and supporting staff with rich experience are appointed in the department.Our vision includes imparting quality education to the students in the domain of Computer Science and Engineering with a focus to create and disseminate the knowledge of problem solving using computers.</p>
+          <h1 class="abouthead">About STACS</h1>
+          <p
+            class="abtst"
+          >Students’ Association of Computer Science (STACS) is the departmental association of both B.Tech and M.Tech students of the Computer Science and Engineering Department. This association will be an integration of students into academic communities at the department level, crossing the course boundary, providing a platform to organise a range of activities with the support and resources of the department. The association will provide opportunities for students to share a range of extra-curricular experiences.</p>
+        </div>
       </div>
+      <div class="about1">
+        <div class="container">
+          <h1 class="abouthead">About CSED</h1>
+          <p
+            class="abtst"
+          >The department of computer science & engineering is the latest addition to NSSCE in the academic year 1999 - 2000. Identification of India as a potential source of computer professionals and keeping in mind the employment potential in this sector, the department aims at providing competent professionals to contribute its share to this Technologies boom. Faculty members and supporting staff with rich experience are appointed in the department.Our vision includes imparting quality education to the students in the domain of Computer Science and Engineering with a focus to create and disseminate the knowledge of problem solving using computers.</p>
+          <hr/>
+        </div>
       </div>
     </div>
   </div>
@@ -21,37 +33,47 @@ export default {}
 </script>
 
 <style>
-.bg {
-  background: #13a59b;
-}
 .bgimg {
-  height: 700px;
+  height: 100%;
   width: 100%;
   background-size: cover;
   background-position: center;
-  background: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5));
 }
-.head-about {
-  color: white;
+.center {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 50%;
+}
+.head1 {
+  font-size: 3vw;
   text-align: center;
-  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
-    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+  color: navy;
 }
-.abtext{
-  color: white;
-  text-align: justify;
-  font-size: large;
+.head2 {
+  font-size: 2vw;
+  text-align: center;
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+  color: navy;
 }
-.abtcse{
+.about {
+  background: #13a59b;
+}
+.about1 {
   background: white;
-  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
 }
-.head-csed{
+.abouthead {
   text-align: center;
+  color: navy;
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+  font-size: 5vw;
 }
-.abtcse{
-  color: black;
+.abtst {
+  font-style: bold;
+  font-size: 1.6vw;
   text-align: justify;
-  font-size:large;
+  color: navy;
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
 }
 </style>
