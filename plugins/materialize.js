a881e61d72815bf189c279446532a763bced49e3
karnan <karnan@pop-os.localdomain>
2020-06-07 13:56:15 +0530

    carousel inducator added

--- plugins/materialize.js
+++ plugins/materialize.js
@@ -5,7 +5,9 @@ const materialize = {
     const elems = document.querySelectorAll('.sidenav')
     M.Sidenav.init(elems, option)
   },
-  carousel (option = {}) {
+  carousel (option = {
+    indicators: true
+  }) {
     const elems = document.querySelectorAll('.carousel')
     M.Carousel.init(elems, option)
   }
