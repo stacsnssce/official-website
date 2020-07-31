e28db28a13bba3b205d602c83f20729e2cc1cd2f
Allen K Abraham <allen@theabstraction.co>
2020-07-31 16:49:11 +0530

    Async fetch in contributors

--- pages/contributors/index.vue
+++ pages/contributors/index.vue
@@ -6,26 +6,26 @@
     <br>
     <br>
     <div class="row">
-      <section v-for="c in contributors.slice().reverse()" :key="c.author.id">
+      <section v-for="c in contributors" :key="c.id">
         <div class="col s12 m4 l4">
           <div class="card-panel hoverable">
             <div class="card-image center-align">
               <progressive-img
                 class="contributor-img circle responsive-image"
-                :src="`${c.author.avatar_url || '/profilepic.png'}`"
+                :src="`${c.avatar_url || '/profilepic.png'}`"
                 placeholder="/imageplaceholder1x1.png"
-                :alt="`${c.author.login}`"
+                :alt="`${c.login}`"
                 :blur="30"
               />
             </div>
             <div class="card-content center-align text">
-              <p v-if="c.author.login" style="font-size:20px">
-                {{ c.author.login }}
+              <p v-if="c.login" style="font-size:20px">
+                {{ c.login }}
               </p>
               <p v-else style="font-size:20px">
-                {{ c.attribute.login }}
+                {{ c.login }}
               </p>
-              <a :href="c.author.html_url">
+              <a :href="c.html_url">
                 View Profile
                 <i class="fa fa-github git circle" />
               </a>
@@ -44,7 +44,7 @@ import axios from 'axios'
 export default {
   async fetch ({ store }) {
     // const psts = []
-    return await axios.get('https://api.github.com/repos/stacsnssce/official-website/stats/contributors')
+    return await axios.get('https://api.github.com/repos/stacsnssce/official-website/contributors')
       .then(async ({ data }) => {
         /* eslint-disable no-console */
         store.commit('Contributors', data)
