a754089efd43c11e8bcba5786194f658ac46e49a
Allen K Abraham <allen@theabstraction.co>
2020-07-31 17:55:49 +0530

    Trash Code removed

--- nuxt.config.js
+++ nuxt.config.js
@@ -72,25 +72,7 @@ const dynamicRoutes = async () => {
       }
     }))
   })
-//   
-//   
-//     Unable to execute
-// 
-//     const dynamicContributorsRoutes = await axios.get('https://api.github.com/repos/stacsnssce/official-website/stats/contributors')
-//     .then(async (data) => {
-//       return await Promise.all(data.data.map(async (dat) => {
-//         return {
-//           route: '/contributors/' + dat.author.login + '/',
-//           payload: await axios.get(dat.author.url)
-//             .then((res) => {
-//               return {
-//                 title: res.data
-//               }
-//             })
-//         }
-//       }))
-//     })
-  
+
   const route = [...dynamicNewsletterRoutes,...dynamicAwardsRoutes, ...dynamicBlogRoutes, ...dynamicActivitiesRoutes]
   return route
 }
